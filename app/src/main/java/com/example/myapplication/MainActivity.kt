1761c96b5497d8fde9a4473aedb95eaf253a29c4
edamour1 <edamourjr@gmail.com>
2020-04-27 06:28:30 -0700

    Pushing to branch_2 practice again.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,7 +8,7 @@ import android.view.MenuItem
 import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
-
+//push dAMN IT
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
